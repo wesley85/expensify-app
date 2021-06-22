c1e32ddf961ffa682ded7de6374fb107ee17db38
Wesley Seitz <wseitz@west.com>
2021-06-22 12:00:14 -0600

    initial commit

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -13,7 +13,7 @@ export class AddExpensePage extends React.Component {
       <div>
       <div className="page-header">
         <div className="content-container">
-          <h1 className="page-header__title">Add Model</h1>
+          <h1 className="page-header__title">Add Expense</h1>
         </div>
       </div>
         <div className="content-container">
